70882764720419ae7c12aa3c276719553011e8c7
Victor Aponte <mrfunnyman@gmail.com>
2013-09-21 19:06:56 -0400

    Added basic gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+var mountFolder = function(connect, dir){
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt){
+
+  // load the plugins used by grunt
+
+
+  // use grunt.registerTask() to register custom tasks
+};
