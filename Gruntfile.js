9d438f79cecd810f4f7e5d968b6cab1e8c66b6b2
Victor Aponte <mrfunnyman@gmail.com>
2013-09-21 22:27:19 -0400

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,15 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    clean: {
+      all: {
+        files: [{
+          dot: true,
+          src: [ './.tmp', './dist' ]
+        }]
+      }
+    },
+
     jshint: {
       jshintrc: '.jshintrc',
       all: [
