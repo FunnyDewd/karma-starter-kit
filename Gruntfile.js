8ebbc765d0ee333112b619ea566acfd821c084dd
Victor Aponte <mrfunnyman@gmail.com>
2013-09-21 21:34:54 -0400

    added jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,27 @@ var mountFolder = function(connect, dir){
 
 module.exports = function(grunt){
 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      jshintrc: '.jshintrc',
+      all: [
+        'Gruntfile.js',
+        'source/**/*.js',
+        '!source/vendor/**/*.js',
+        'test/**/*.js'
+      ]
+    }
+  });
+
   // load the plugins used by grunt
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-karma');
 
 
   // use grunt.registerTask() to register custom tasks
